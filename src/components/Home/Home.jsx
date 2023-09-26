228687af9de9c2cfc2863f96d0dfe722704ac938
vinhchis <vinhchislee@gmail.com>
2023-09-26 17:38:56 +0700

    Update json for ranges

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,37 @@
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
-import ListRange from "../ListRange/ListRange";
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import CarouselImg_01 from "../../assets/image/carousel/background-carousel.jpg";
+import CarouselImg_02 from "../../assets/image/carousel/background-carousel.jpg";
+import CarouselImg_03 from "../../assets/image/carousel/background-carousel.jpg";
+
+import "./home.css"
 
 function Home() {
-  return ( 
-    <>
-      HomePage
-    </>
-   );
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img src={CarouselImg_01} alt="First slide" width="100%"/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={CarouselImg_02} alt="Second slide" width="100%" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={CarouselImg_03} alt="Thirst slide" width="100%" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
 export default Home;
--- src/components/ListRange/ListRange.jsx
+++ src/components/ListRange/ListRange.jsx
@@ -1,68 +1,94 @@
 import { CardGroup } from "react-bootstrap";
-// import Range from "./Cards/Range/Range";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import SearchBar from "../SearchBar/SearchBar";
 
+import RangesDB from "../../data/ranges.json"
 
+import ConvertibleImg from "../../assets/image/ranges/Convertible.svg";
+import CoupleImg from "../../assets/image/ranges/Couple.svg";
+import HatchbackImg from "../../assets/image/ranges/Hatchback.svg";
+import MPVImg from "../../assets/image/ranges/MPV.svg";
+import SedanImg from "../../assets/image/ranges/Sedan.svg";
 import SUVImg from "../../assets/image/ranges/SUV.svg";
+import TrunkImg from "../../assets/image/ranges/Trunk.svg";
+import VANImg from "../../assets/image/ranges/VAN.svg";
+import WagonImg from "../../assets/image/ranges/Wagon.svg";
 
-function Range() {
-  const imgLink = "../../../assets/img/ranges";
-  let Range = {
-    id: 1,
-    name: "SUV",
-    quantity : 12,
-    desc: "Versatile, spacious, and rugged vehicles designed for diverse lifestyles, blending car comfort with off-road capability.",
-  };
+
+
+import { useState } from "react";
+
+function Range({range}) {
+  const imgLink = `../../assets/img/ranges/${range.name}.png`;
+  // let range = {
+  //   id: 1,
+  //   name: "SUV",
+  //   quantity : 12,
+  //   desc: "Versatile, spacious, and rugged vehicles designed for diverse lifestyles, blending car comfort with off-road capability.",
+  // };
 
   return (
     <Card className="text-center" style={{ width: "18rem" }}>
       {/* <Card.Header>{`${range.count} cars`}</Card.Header> */}
-      <Card.Img variant="top" src={SUVImg} />
+      <Card.Img variant="top" src={imgLink} />
       <Card.Body>
-        <Card.Title>{Range.name}</Card.Title>
-        <Card.Text class="text-start">{Range.desc}</Card.Text>
+        <Card.Title>{range.name}</Card.Title>
+        <Card.Text class="text-start">{range.desc}</Card.Text>
         <Button variant="primary">List Car</Button>
       </Card.Body>
-      <Card.Footer className="text-muted">{Range.quantity} items</Card.Footer>
+      <Card.Footer className="text-muted">n items</Card.Footer>
     </Card>
   );
 }
 
 function ListRange() {
+  const [ranges, setRanges] = useState(RangesDB);
+
+  // console.log(ranges)
+
   return (
     <div className="list-range col-10 offset-1">
       <SearchBar />
+      {/* <Range range={ranges[0]}/>
+      {ranges.map((x,range) => (
+        x % 4 ? null : null,
+        <Range range={range}/>
+       ))} */}
       <div>
         <Row className="my-4">
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[0]} />
           </Col>
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[1]} />
           </Col>
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[2]} />
           </Col>
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[3]} />
           </Col>
         </Row>
         <Row className="my-4">
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[4]} />
           </Col>
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[5]} />
           </Col>
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[6]} />
           </Col>
           <Col ms={3}>
-            <Range />
+            <Range range={ranges[7]} />
+          </Col>
+        </Row>
+        <Row className="my-4">
+          <Col ms={3}>
+            <Range range={ranges[8]} />
           </Col>
         </Row>
       </div>
