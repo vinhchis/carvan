063214be5236011b39df4f64edf2149829034cd6
Vinh Chi <134524234+vinhchis@users.noreply.github.com>
2023-10-04 23:33:45 +0700

    change to hash router in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 
 );
